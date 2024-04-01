8c3c97bee318290d6f5ed61bfa5a08ec5a228d43
Vadim Volkov <vvadim.vvolkov.ps4@gmail.com>
2024-04-01 23:48:57 +0600

    merge finding public and private key in 1 function; add checking if e and euler function are coprime

--- RSA/find_keys.py
+++ RSA/find_keys.py
@@ -2,22 +2,21 @@ from math import gcd
 from gmpy2 import invert
 from random import randint
 
-def find_coprime_number(x):
-    while True:
-       coprime_num = randint(2, x-1)
-       if gcd(coprime_num, x) == 1:
-          return coprime_num
 
-def find_inverse_number_by_mod(x, mod):
-    return invert(x, mod)
+def find_rsa_keys(x):
+    while True:
+       d = randint(2, x-1)
+       if gcd(d, x) == 1:
+        e = invert(d, x)
+        if gcd(e, x) == 1:
+          return(int(e), d)
 
 p = 127
 q = 263
 n = p*q
 euler_func = (p-1)*(q-1)
 
-d_list = [find_coprime_number(euler_func) for i in range(3)]
-e_list = list((int(find_inverse_number_by_mod(num, euler_func)) for num in d_list))
+keys_list = [find_rsa_keys(euler_func) for i in range(3)]
 
-print('e:', list([e, n] for e in e_list))
-print('d:', list([d, n] for d in d_list))
+print('(e, n):', list([key[0], n] for key in keys_list))
+print('(d, n):', list([key[1], n] for key in keys_list))
